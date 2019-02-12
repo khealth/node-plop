86d342b8057597eedfea3f1c566e057d57dff957
Max Komarychev <maxkomarychev@gmail.com>
2019-02-12 17:33:42 +0200

    Use copy of the data

--- src/generator-runner.js
+++ src/generator-runner.js
@@ -116,10 +116,11 @@ export default function (plopfileApi, flags) {
 		let cfgData = cfg.data || {};
 		// data can also be a function that returns a data object
 		if (typeof cfgData === 'function') { cfgData = yield cfgData(); }
+		const actionData = { ...data }
 		Object.keys(cfgData || {}).forEach(k => {
-			if (typeof data[k] === 'undefined') { data[k] = cfgData[k]; }
+			if (typeof actionData[k] === 'undefined') { actionData[k] = cfgData[k]; }
 		});
-		return yield Promise.resolve(action(data, cfg, plopfileApi)).then(
+		return yield Promise.resolve(action(actionData, cfg, plopfileApi)).then(
 			// show the resolved value in the console
 			result => ({
 				type, path: (typeof result === 'string'
